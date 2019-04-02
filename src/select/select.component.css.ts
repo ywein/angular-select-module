5648b2d432618d53b1723be21ce0ac5b327e75e3
Ywein <eugene@webmind28.com>
2019-04-02 06:06:41 +0300

    postcss nested fix

--- src/select/select.component.css.ts
+++ src/select/select.component.css.ts
@@ -3,7 +3,8 @@ export const styles = ` .angular-select {
   -webkit-box-sizing: border-box;
           box-sizing: border-box;
   font-size: 18px;
-  &__select-box {
+}
+  .angular-select__select-box {
     height: 40px;
     line-height: 40px;
     border-radius: 2px;
@@ -14,38 +15,32 @@ export const styles = ` .angular-select {
     cursor: pointer;
     overflow: hidden;
   }
-}
-@media (max-width: 767px) {
-  .angular-select {
-    &__select-box {
-      display: none;
-    }
+  @media (max-width: 767px) {
+  .angular-select__select-box {
+      display: none
   }
-}
-.angular-select {
-  &__select-box:after {
-    content: ' ';
-    width: 0;
-    height: 0;
-    border-left: 6px solid transparent;
-    border-right: 6px solid transparent;
-    border-top: 6px solid #000000;
-    position: absolute;
-    right: 4px;
-    top: 50%;
-    -webkit-transform: translate(0, -50%);
-            transform: translate(0, -50%);
-  }
-  &__select-box {
-    &--open:after {
-      content: '';
-      display: none;
     }
-    &--open.angular-select__select-box--with-search {
+  .angular-select__select-box:after {
+      content: ' ';
+      width: 0; 
+      height: 0; 
+      border-left: 6px solid transparent;
+      border-right: 6px solid transparent;
+      border-top: 6px solid #000000;
+      position: absolute;
+      right: 4px;
+      top: 50%;
+      -webkit-transform: translate(0, -50%);
+              transform: translate(0, -50%);
+    }
+  .angular-select__select-box--open:after {
+        content: '';
+        display: none;
+      }
+  .angular-select__select-box--open.angular-select__select-box--with-search {
       padding: 0;
     }
-  }
-  &__search {
+  .angular-select__search {
     width: 100%;
     height: 100%;
     font-size: 18px;
@@ -56,15 +51,15 @@ export const styles = ` .angular-select {
     border: 0;
     padding: 0 4px;
   }
-  &__select-box-clear:before {
-    content: '✕';
-    position: absolute;
-    right: 20px;
-    top: 50%;
-    -webkit-transform: translate(0, -50%);
-            transform: translate(0, -50%);
-  }
-  &__options-list {
+  .angular-select__select-box-clear:before {
+      content: '✕';
+      position: absolute;
+      right: 20px;
+      top: 50%;
+      -webkit-transform: translate(0, -50%);
+              transform: translate(0, -50%);
+    }
+  .angular-select__options-list {
     display: none;
     list-style: none;
     -webkit-box-sizing: border-box;
@@ -82,26 +77,25 @@ export const styles = ` .angular-select {
     border: 1px solid rgba(0, 0, 0, .15);
     -webkit-box-shadow: 0 6px 12px rgba(0, 0, 0, .175);
             box-shadow: 0 6px 12px rgba(0, 0, 0, .175);
-    &--open {
+  }
+  .angular-select__options-list--open {
       display: block;
     }
-  }
-  &__option {
+  .angular-select__option {
     cursor: pointer;
     height: 24px;
     line-height: 24px;
     padding: 0 10px;
-    &:hover, &--selected {
+  }
+  .angular-select__option:hover, .angular-select__option--selected {
       background: #428BCA;
       color: #ffffff;
     }
-  }
-}
 .angular-select select {
   display: none;
 }
 @media (max-width: 767px) {
-  .angular-select select {
-    display: block;
-  }
-}`;
+.angular-select select {
+    display: block
+}
+  }`;
